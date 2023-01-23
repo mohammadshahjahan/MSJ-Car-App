0324facf52f4c0c44310440e08d4ca54fe59ae4c
Shahjahan <md.shahjahan786pubg@gmail.com>
2023-01-23 18:15:34 +0530

    Final Changes

--- carapp/carapp/settings.py
+++ carapp/carapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-np%dr-38i)i_l)n^=rl=3(^uow8f(@hl3!5828l0%o^+)qd_z8"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
@@ -91,10 +92,10 @@ WSGI_APPLICATION = "carapp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": 'carzone_db',
-        "USER": 'postgres',
-        "PASSWORD": 'qwerty@1234',
+        "ENGINE": config('ENGINE'),
+        "NAME": config('NAME'),
+        "USER": config('USER'),
+        "PASSWORD": config('PASSWORD'),
         "HOST": 'localhost',
     }
 }
