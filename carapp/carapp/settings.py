aa70a579e649380173862caa28210ab509da6cea
Shahjahan <md.shahjahan786pubg@gmail.com>
2023-05-31 21:56:35 +0530

    making some changes for deployment

--- carapp/carapp/settings.py
+++ carapp/carapp/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 LOGIN_REDIRECT_URL = 'dashboard'
 # Application definition
